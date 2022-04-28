791cdb74678dba7952568c776d838352e5966d41
acamp <acampbell8610@gmail.com>
2022-04-27 20:42:31 -0500

    addd manifest.json

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+
+new WebpackPwaManifest({
+    name: "Budget Tracker",
+    short_name: "$ Tracker",
+    description: "An app that allows you to budget your money.",
+    start_url: "../index.html",
+    background_color: "#01579b",
+    theme_color: "#ffffff",
+    fingerprints: false,
+    inject: false,
+    icons: [{
+      src: path.resolve("/public/icons/icon-512x512.png"),
+      sizes: [96, 128, 192, 256, 384, 512],
+      destination: path.join("public", "icons")
+    }]
+  })
